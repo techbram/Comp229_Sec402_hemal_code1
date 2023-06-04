83e28dfb6a3e6a82b98894979728132a15a7f53a
techbram <bramcoder@gmail.com>
2023-06-04 19:32:32 -0400

    code move to another repo
    code move to another repo

---
+++ public/Scripts/app.js
@@ -0,0 +1,10 @@
+//IFIE function. Immidiately invoked Function Expression
+
+(function () {
+    
+    function start(){
+        console.log('App started');        
+    }
+    window.addEventListener('load', start);
+  }
+)();
